3791841d5158fe0ef02e96a6270437f49fd4e5ec
Matt Toensing <matthew.toensing@gmail.com>
2021-08-06 09:13:07 -0700

    change method name

--- app/models/garden.rb
+++ app/models/garden.rb
@@ -1,7 +1,7 @@
 class Garden < ApplicationRecord
   has_many :plots
 
-  def plants_less_than_hundred
+  def plant_names_less_than_hundred
     self.plots.joins(:plants).select(:plants).distinct.where('days_to_harvest <= ?', 100).pluck(:name)
   end
 end
